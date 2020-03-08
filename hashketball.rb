e1f1cd44b08116fbbea4b460b6961384c0ff809f
George Cooper <coopsgeorge@gmail.com>
2020-03-08 18:13:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -234,7 +234,7 @@ person_with_biggest_feet = shoe_size_hash[-1][0]
 end
 
 def most_points_scored
-  
+
   shoe_size_hash = {}
 
     game_hash.each do |home_away, team|
@@ -252,5 +252,5 @@ def most_points_scored
   shoe_size_hash = shoe_size_hash.sort { |l, r| l[1]<=>r[1] }
 
   person_with_biggest_feet = shoe_size_hash[-1][0]
-  
+
 end

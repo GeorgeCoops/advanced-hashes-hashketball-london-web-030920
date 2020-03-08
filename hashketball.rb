2a9d9a48e0935fa21631a082fc2cc22d6970461e
George Cooper <coopsgeorge@gmail.com>
2020-03-08 18:09:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -220,8 +220,6 @@ shoe_size_hash = shoe_size_hash.sort { |l, r| l[1]<=>r[1] }
 
 person_with_biggest_feet = shoe_size_hash[-1][0]
 
-player_with_biggest_shoes =
-
   game_hash.each do |home_away, team|
     team.each do |attribute, data|
       if attribute == :players
@@ -233,4 +231,8 @@ player_with_biggest_shoes =
       end
     end
   end
+end
+
+def most_points_scored 
+
 end
